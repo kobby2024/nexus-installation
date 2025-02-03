9f6463e80968168b6b49fa5ae2a6c3fca625cf95
kobby2024 <161094250+kobby2024@users.noreply.github.com>
2025-02-03 14:30:05 -0500

    Update nexus_install.sh

--- nexus_install.sh
+++ nexus_install.sh
@@ -6,32 +6,23 @@
 #1. Create nexus user to manage the nexus
 # As a good security practice, Nexus is not advised to run nexus service as a root user, so create a new user called nexus and grant sudo access to manage nexus services as follows.
 
-useradd nexus
+sudo useradd nexus
 
 #4 Give sudo access to nexus user
-
 sudo echo "nexus ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/nexus
 sudo su - nexus
-
 cd /opt
-
 # 1.Install prerequisit: JAVA, git, unzip
 
 sudo yum install wget git nano unzip -y
-sudo yum install java-11-openjdk-devel java-1.8.0-openjdk-devel -y
-
+sudo yum install java-17-openjdk-devel -y
 
 # 2. Download nexus software and extract it (unzip)
-
 sudo wget http://download.sonatype.com/nexus/3/nexus-3.15.2-01-unix.tar.gz 
-
 sudo tar -zxvf nexus-3.15.2-01-unix.tar.gz
-mv /opt/nexus-3.15.2-01 /opt/nexus
-
+sudo mv /opt/nexus-3.15.2-01 /opt/nexus
 
 #5 Change the owner and group permissions to /opt/nexus and /opt/sonatype-work directories.
-
-
 sudo chown -R nexus:nexus /opt/nexus
 sudo chown -R nexus:nexus /opt/sonatype-work
 sudo chmod -R 775 /opt/nexus
@@ -39,11 +30,10 @@ sudo chmod -R 775 /opt/sonatype-work
 
 #6 Open /opt/nexus/bin/nexus.rc file and  uncomment run_as_user parameter and set as nexus user.
 
-vi /opt/nexus/bin/nexus.rc
+sudo vim /opt/nexus/bin/nexus.rc
 run_as_user="nexus"
 
 #7 CONFIGURE NEXUS TO RUN AS A SERVICE 
-
 sudo ln -s /opt/nexus/bin/nexus /etc/init.d/nexus
 
 #9 Enable and start the nexus services
